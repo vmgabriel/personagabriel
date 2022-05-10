e8ce4deea4357547da6166eb4a7c628e7ffd18b6
Gabriel <vmgabriel96@gmail.com>
2022-05-10 16:15:58 -0500

    Feat: First Commit With All

---
+++ config/app.py
@@ -0,0 +1,5 @@
+"""App Settings"""
+
+MDA_APPS = [
+    "personagabriel.apps.personagabrielConfig",
+]
---
+++ personagabriel/models/__init__.py
@@ -0,0 +1,6 @@
+"""All Models Personagabriel"""
+
+from .steres import Steres
+from .user import User
+from .preducts import Preducts
+from .sheppingcart import Sheppingcart
